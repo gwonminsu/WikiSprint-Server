61b334fc5c94ffcaa766d6570b632793759824d3
gwonminsu <kd0615@naver.com>
2026-04-10 02:59:08 +0900

    cors allowed origins 수정

--- src/main/java/com/wikisprint/server/global/config/SecurityConfig.java
+++ src/main/java/com/wikisprint/server/global/config/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:5969");
+        configuration.addAllowedOrigin("http://13.209.255.179:5969");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
